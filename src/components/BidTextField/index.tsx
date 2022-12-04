34304af7a6d182e23513477fdd029fbdd6a68d7f
logesh2496 <logesh.r24@gmail.com>
2022-12-04 21:36:52 +0530

    fix: get weth and enable

--- src/components/BidTextField/index.tsx
+++ src/components/BidTextField/index.tsx
@@ -1,4 +1,10 @@
-import { Box, TextField, CircularProgress, Typography } from "@mui/material";
+import {
+  Box,
+  TextField,
+  CircularProgress,
+  Typography,
+  Link,
+} from "@mui/material";
 import { BigNumber, ethers } from "ethers";
 import { motion } from "framer-motion";
 import { useState } from "react";
@@ -26,55 +32,65 @@ const BidTextField = ({
     setBidValue(BigNumber.from(ethers.utils.parseEther(e.target.value)));
   };
   return (
-    <Box display={"flex"} alignItems="center">
-      <TextField
-        type={"number"}
-        onChange={onBidChange}
-        value={bidValueUi}
-        InputProps={{
-          inputProps: { min: 0.1, step: "0.1" },
-        }}
-      />
-      <motion.div
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.8 }}
-        style={{
-          background: `linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)`,
-          borderRadius: "6px",
-          // width: "150px",
-          padding: "8px 20px",
-          // height: "80px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          cursor: "pointer",
-          userSelect: "none",
-          MozUserSelect: "none",
-          msUserSelect: "none",
-        }}
-        onClick={() => {
-          const _nextStepBid = previousBid.add(previousBid.mul(10).div(100));
-          if (bidValue.lte(_nextStepBid)) {
-            setShowAlertMessage(
-              `Place higher bid than ${ethers.utils.formatEther(
-                _nextStepBid
-              )} WETH`
-            );
-            return;
-          }
-          if (!!auctionObj) {
-            onBidNow(bidValue);
-          }
-        }}
+    <Box display={"flex"} flexDirection="column" alignItems={"start"}>
+      <Box display={"flex"} alignItems="center">
+        <TextField
+          type={"number"}
+          onChange={onBidChange}
+          value={bidValueUi}
+          InputProps={{
+            inputProps: { min: 0.1, step: "0.1" },
+          }}
+        />
+        <motion.div
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.8 }}
+          style={{
+            background: `linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)`,
+            borderRadius: "6px",
+            // width: "150px",
+            padding: "8px 20px",
+            // height: "80px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            cursor: "pointer",
+            userSelect: "none",
+            MozUserSelect: "none",
+            msUserSelect: "none",
+          }}
+          onClick={() => {
+            const _nextStepBid = previousBid.add(previousBid.mul(10).div(100));
+            if (bidValue.lte(_nextStepBid)) {
+              setShowAlertMessage(
+                `Place higher bid than ${ethers.utils.formatEther(
+                  _nextStepBid
+                )} WETH`
+              );
+              return;
+            }
+            if (!!auctionObj) {
+              onBidNow(bidValue);
+            }
+          }}
+        >
+          {isLoading ? (
+            <CircularProgress size={36} color="secondary" />
+          ) : (
+            <Typography variant="h6" align="center">
+              BID NOW
+            </Typography>
+          )}
+        </motion.div>
+      </Box>
+      <Link
+        href="//app.uniswap.org/#/tokens/ethereum/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+        target={"_blank"}
+        color="secondary"
+        sx={{ mt: 1 }}
       >
-        {isLoading ? (
-          <CircularProgress size={36} color="secondary" />
-        ) : (
-          <Typography variant="h6" align="center">
-            BID NOW
-          </Typography>
-        )}
-      </motion.div>
+        Get WETH
+      </Link>
     </Box>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,7 @@ import { Web3ReactProvider } from "@web3-react/core";
 import { ethers } from "ethers";
 // import NonVisualizer from "./NonVisualizer";
 import Auction from "./Auction";
+import NonVisualizer from "./NonVisualizer";
 // import NonVisualizer from "./NonVisualizer";
 
 WebFont.load({
@@ -93,8 +94,8 @@ root.render(
             {/* <Route path="/" element={<MarketPlace />} />
             <Route path="/upload" element={<App />} />
             <Route path="/mackenzie" element={<NonVisualizer trackIdx={0} />} /> */}
-            {/* <Route path="/" element={<NonVisualizer trackIdx={1} />} /> */}
-            <Route path="/" element={<Navigate to="/auction/8" />} />
+            <Route path="/" element={<NonVisualizer trackIdx={1} />} />
+            {/* <Route path="/" element={<Navigate to="/auction/8" />} /> */}
             <Route path="/auction" element={<Navigate to="/auction/8" />} />
             <Route path="/auction/:id" element={<Auction />} />
           </Routes>
